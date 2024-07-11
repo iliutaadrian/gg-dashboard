42ba1abf93b91b9306bd0d430af042b054d039e9
iliutaadrian <iliuta.podoleanu@golfgenius.com>
2024-07-11 10:56:56 +0300

    feat: cronjob docke, public middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // publicRoutes: ["/", "/contact", "/summary-list", "/api/webhook"],
+  publicRoutes: ["/api/jenkins/cron"],
 });
 
 export const config = {
